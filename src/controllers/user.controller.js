8ffe1361590b5d0dec9685223260807d327ff8d0
kush <kushkumarking098765@gmail.com>
2025-04-13 16:05:43 +0530

    user registeration created in routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,16 +1,63 @@
-import { response } from "express";
 import asyncHandler from "../utils/asyncHandler.js";
+import User from "../models/user.model.js";
+import apiError from "../utils/apiError.js";
+import apiResponse from "../utils/apiResponse.js";
+import { uploadtoCloudinary } from "../utils/cloudinary.js";
 
-const registeruser = asyncHandler(async (req, res) =>{
-  res.status(200).json({
-    success: true,
-    message: "User registered successfully",
+const registeruser = asyncHandler(async (req, res) => {
+  // Check if the request body contains all required fields
+  const { username, email, fullname, password } = req.body;
+  if (!username || !email || !fullname || !password) {
+    throw new apiError(400, "All fields are required");
+  }
+  // Check if the user already exists
+  const existingUser = await User.findOne({
+    $or: [{ username }, { email }],
   });
+  // If the user already exists, throw an error
+  if (existingUser) {
+    throw new apiError(409, "User with Email or Username already exists");
+  }
+  // get the file names from the request
+  const avatarlocalpath = req.files?.avatar[0].path;
+  const coverimagelocalpath = req.files?.coverimage[0].path;
+
+  // Check if the avatar and cover image are provided
+  if (!avatarlocalpath) {
+    throw new apiError(400, "Avatar is required");
+  }
+
+  //upload the avatar and cover image to Cloudinary
+  const avatar = await uploadtoCloudinary(avatarlocalpath);
+  const coverimage = await uploadtoCloudinary(coverimagelocalpath);
+
+  if (!avatar) {
+    throw new apiError(500, "Failed to upload avatar to Cloudinary");
+  };
+
+  //create a new user
+ const user = await User.create({
+    username : username.toLowerCase(),
+    email : email.toLowerCase(),
+    fullname,
+    password,
+    avatar,
+    coverimage : coverimage || "",
+  });
+  // Check if the user was created successfully
+  if (!user) {
+    throw new apiError(500, "Failed to create user");
+  };  
+   
+  // Send a success response with the created user data
+  const createdUser = await User.findById(user._id).select("-password -refreshtoken");
+
+  if (!createdUser) {
+    throw new apiError(500, "something went wrong , please try again later");
+  };
+  // Send a success response with the created user data
+  res.status(201).json(new apiResponse(201, createdUser, "User created successfully"));
+
 });
-
-
-
-
-
 
 export { registeruser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,16 @@
-import express from 'express';
-import { registeruser } from '../controllers/user.controller.js';
-
+import express from "express";
+import { registeruser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const userRouter = express.Router();
 
-
-userRouter.post('/register', registeruser);
-
-
-
-
+userRouter.post(
+  "/register",
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverimage", maxCount: 1 },
+  ]),
+  registeruser
+);
 
 export default userRouter;
-
